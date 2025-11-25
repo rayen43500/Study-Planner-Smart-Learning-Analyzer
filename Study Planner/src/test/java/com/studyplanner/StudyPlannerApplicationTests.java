96cefc869962bd525bee67595217173200d15cdf
rayen43500 <rayen.9b7@gmail.com>
2025-11-25 18:45:42 +0100

    first push

---
+++ Study Planner/src/test/java/com/studyplanner/StudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
+
